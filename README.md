d63198a9bcbe70366f358a2971c256bcaa68eac6
bigbimbs <60890425+bigbimbs@users.noreply.github.com>
2022-03-18 06:27:43 +0100

    Add files from my local machine via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# This Project was built using main JS with other libraries D3.js and Chart.JS
+
+## You do not need to install any package just make sure you are connected to the internet because this project made use of CDN instead of packages.
+
+## Always make sure the dataset csv file is in the root directory of the project folder and host it via localhost with Xammp in the htdocs folder
---
+++ app.js
@@ -0,0 +1,98 @@
+const chartData = "http://localhost/dataset.csv";
+
+async function moses() {
+  await d3.csv(chartData).then((datapoints) => {
+    console.log(datapoints);
+    const area = [];
+    const year = [];
+    const country = [];
+    const gdp = [];
+    const population = [];
+    const populationDensity = [];
+    const roadsPerMillion = [];
+    const totalRoads = [];
+    const vehicleOwnership = [];
+    for (i = 0; i < datapoints.length; i++) {
+      area.push(datapoints[i].Area);
+      country.push(datapoints[i].Country);
+      gdp.push(datapoints[i].GDPperCapital);
+      population.push(datapoints[i].Population);
+      populationDensity.push(datapoints[i].PopulationDensity);
+      roadsPerMillion.push(datapoints[i].RoaddeathsperMillionInhabitants);
+      totalRoads.push(datapoints[i].Totalroaddeaths);
+      vehicleOwnership.push(datapoints[i].VehicleOwnership);
+      year.push(datapoints[i].Year);
+    }
+    const labels = country;
+    console.log(area);
+
+    const data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "Road deaths per Million Inhabitants",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: roadsPerMillion,
+        },
+        {
+          label: "Total road deaths",
+          backgroundColor: "red",
+          borderColor: "red",
+          data: totalRoads,
+        },
+        {
+          label: "Vehicle Ownership",
+          backgroundColor: "grey",
+          borderColor: "grey",
+          data: vehicleOwnership,
+        },
+        {
+          label: "Population Density",
+          backgroundColor: "lightblue",
+          borderColor: "lightblue",
+          data: populationDensity,
+        },
+        {
+          label: "GDP per Capital (0)",
+          backgroundColor: "green",
+          borderColor: "green",
+          data: gdp,
+        },
+        {
+          label: "Population (0,000)",
+          backgroundColor: "darkblue",
+          borderColor: "darkblue",
+          data: population,
+        },
+        {
+          label: "Area",
+          backgroundColor: "yellow",
+          borderColor: "yellow",
+          data: area,
+        },
+        {
+          label: "Year",
+          backgroundColor: "lightgray",
+          borderColor: "lightgray",
+          data: year,
+        },
+      ],
+    };
+
+    const config = {
+      type: "bar",
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+    const myChart = new Chart(document.getElementById("myChart"), config);
+  });
+}
+
+moses();
